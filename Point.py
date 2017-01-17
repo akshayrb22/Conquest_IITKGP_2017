ee090e867211737543b2d15908a277a243953a73
AKSHAYRB\Akshay <akshay.bhat981222@gmail.com>
2017-01-18 00:01:20 +0530

    --Added point class

---
+++ Point.py
@@ -0,0 +1,13 @@
+##This is a file to define a class point which will return the coordinate
+##It can also return the x & y coordinates separately
+
+class Point(object):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+    def get_coordinate(self):
+        return (self.x, self.y)
+    
+    
