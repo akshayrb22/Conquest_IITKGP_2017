fa151c45afceabd668ce5f986242a731f753918a
Akshay Ram Bhat <akshay.bhat981222@gmail.com>
2017-10-28 21:52:43 +0530

    Fixing small stuff.

--- BluetoothController.py
+++ BluetoothController.py
@@ -13,79 +13,79 @@ from FindDirectionality import Direction, MovementFunctions, Orientation
 
 
 class BluetoothController(object):
-    target_name = "HC-05"
-    target_address = "20:16:07:05:28:54"
-    nearby_devices = None
-    is_connected = False
-    port = 1
-    sock = None
-    prevCommand = ""
-    command = "s"
-    #def __init__(se, target_name, target_address,target_port):
-    #    target_name = "KAIZEN""
-    #    target_address = target_address
-    #    port = target_port 
+	target_name = "HC-05"
+	target_address = "20:16:07:05:28:54"
+	nearby_devices = None
+	is_connected = False
+	port = 1
+	sock = None
+	prevCommand = ""
+	command = "s"
+	#def __init__(se, target_name, target_address,target_port):
+	#    target_name = "KAIZEN""
+	#    target_address = target_address
+	#    port = target_port 
 
-    @staticmethod
-    def connect():
-        print 'Bluetooth Controller Searching for devices... '
-        nearby_devices = bluetooth.discover_devices()
-        for bluetooth_address in nearby_devices:
-            if BluetoothController.target_address == bluetooth_address:
-                BluetoothController.is_connected = True
-                BluetoothController.target_address = bluetooth_address
-                if BluetoothController.target_address is not None:
-                    print "Bluetooth Controller  found target with address ", BluetoothController.target_address
-                    BluetoothController.connect_to_slave()
-                                      
-                else:
-                    print "could not find target bluetooth device nearby"                  
-                break
-    @staticmethod
-    def connect_to_slave():
-        BluetoothController.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        print "Bluetooth Controller  >> Connecting to Slave...."
-        BluetoothController.sock.connect((BluetoothController.target_address, BluetoothController.port))
-        print"Bluetooth Controller conected success"
-    @staticmethod    
-    def disconnect():
-        BluetoothController.sock.close()
-    @staticmethod
-    def send_command(command,message = None):
+	@staticmethod
+	def connect():
+		print 'Bluetooth Controller Searching for devices... '
+		nearby_devices = bluetooth.discover_devices()
+		for bluetooth_address in nearby_devices:
+			if BluetoothController.target_address == bluetooth_address:
+				BluetoothController.is_connected = True
+				BluetoothController.target_address = bluetooth_address
+				if BluetoothController.target_address is not None:
+					print "Bluetooth Controller  found target with address ", BluetoothController.target_address
+					BluetoothController.connect_to_slave()
+									  
+				else:
+					print "could not find target bluetooth device nearby"                  
+				break
+	@staticmethod
+	def connect_to_slave():
+		BluetoothController.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		print "Bluetooth Controller  >> Connecting to Slave...."
+		BluetoothController.sock.connect((BluetoothController.target_address, BluetoothController.port))
+		print"Bluetooth Controller conected success"
+	@staticmethod    
+	def disconnect():
+		BluetoothController.sock.close()
+	@staticmethod
+	def send_command(command,message = None):
 
-        #send all command
-        #print message
-        if command != BluetoothController.prevCommand:
-            if message == None:
-                print "Sent command " + command
-            else:
-                print message
-        if BluetoothController.is_connected == True:
-            BluetoothController.sock.send(command)
-            BluetoothController.prevCommand = command
-        
-        #     if BluetoothController.is_connected == True:
-        #         BluetoothController.prevCommand = command
-        #         BluetoothController.sock.send(command)
-        return 
+		#send all command
+		#print message
+		if command != BluetoothController.prevCommand:
+			if message == None:
+				print "Sent command " + command
+			else:
+				print message
+		if BluetoothController.is_connected == True:
+			BluetoothController.sock.send(command)
+			BluetoothController.prevCommand = command
+		
+		#     if BluetoothController.is_connected == True:
+		#         BluetoothController.prevCommand = command
+		#         BluetoothController.sock.send(command)
+		return 
 
 def parallelProcess(command):
-    BluetoothController.connect()
-    while True:
-        print "sending  command " + command
-        BluetoothController.send_command(command)
-        sleep(1)
+	BluetoothController.connect()
+	while True:
+		print "sending  command " + command
+		BluetoothController.send_command(command)
+		sleep(1)
 
 if __name__ == '__main__':
-    #bluetoothController = BluetoothController("KAIZEN","B8:27:EB:26:F6:A4",1)
-    command = "s"
-    #bluetoothProcess = Process(target=parallelProcess,args=(command))
-    #bluetoothProcess.start()
-    #bluetoothProcess.join()
-    BluetoothController.connect()
-    
-    while True:
-        command = raw_input("Enter command: ")
-        print " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + command
-        #BluetoothController.command = command
-        BluetoothController.send_command(command)
+	#bluetoothController = BluetoothController("KAIZEN","B8:27:EB:26:F6:A4",1)
+	command = "s"
+	#bluetoothProcess = Process(target=parallelProcess,args=(command))
+	#bluetoothProcess.start()
+	#bluetoothProcess.join()
+	BluetoothController.connect()
+	
+	while True:
+		command = raw_input("Enter command: ")
+		print " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " + command
+		#BluetoothController.command = command
+		BluetoothController.send_command(command)
--- Config.py
+++ Config.py
@@ -1,40 +1,40 @@
 class Config(object):
-    
-    
-    #grid size > devide the arena into given value. Higher the value slower path finding and more resolution (Not required!! )
-    gridSize = 35 #CHANGE THIS for new AREANA (3m X 3m) may be 35?
+	
+	
+	#grid size > devide the arena into given value. Higher the value slower path finding and more resolution (Not required!! )
+	gridSize = 35 #CHANGE THIS for new AREANA (3m X 3m) may be 35?
 
-    #path optimizer value
-    pathTolerance = 1.3
-    # boundry of obtstacle
-    obstacleRange =  4 #may be 4? for 3X3 arena | Reduce this if you get came_from[target] error!
+	#path optimizer value
+	pathTolerance = 1.3
+	# boundry of obtstacle
+	obstacleRange =  4 #may be 4? for 3X3 arena | Reduce this if you get came_from[target] error!
 
-    #max speed 255
-    # reduce for 200 RPM Motors!!!!!!
-    moveSpeed = 250
-    moveSpeedNear = moveSpeed - 70 #When bot is closer to the target
-    turnSpeed = 240
+	#max speed 255
+	# reduce for 200 RPM Motors!!!!!!
+	moveSpeed = 250
+	moveSpeedNear = moveSpeed - 70 #When bot is closer to the target
+	turnSpeed = 240
 
-    reduceSpeedAt = 150
-    # + or - angle for target
-    targetAngleRange = 8
+	reduceSpeedAt = 150
+	# + or - angle for target
+	targetAngleRange = 8
 
-    goToResourceTwice = False   #if True Bot will go to resource twice
+	goToResourceTwice = False   #if True Bot will go to resource twice
 
-    resourcePositionRange = 25  # within 25X25, looks fine for current arena
-    
-    
-    mapRatio = 1.33
-    FrameWidth = 800 * mapRatio
-    FrameHeight = 800
+	resourcePositionRange = 25  # within 25X25, looks fine for current arena
+	
+	
+	mapRatio = 1.33
+	FrameWidth = 800 * mapRatio
+	FrameHeight = 800
 
-    #static DO NOT CHANGE !!!
-    mappedWidth = gridSize * mapRatio
-    mappedHeight = gridSize
-    findPathOnce = True
-    obstacleCount = 0
-    obstacleList = None
-    resourceList = None
-    obstacleBoundingPointList = []
-    startTime = None
-    endTime = None
+	#static DO NOT CHANGE !!!
+	mappedWidth = gridSize * mapRatio
+	mappedHeight = gridSize
+	findPathOnce = True
+	obstacleCount = 0
+	obstacleList = None
+	resourceList = None
+	obstacleBoundingPointList = []
+	startTime = None
+	endTime = None
